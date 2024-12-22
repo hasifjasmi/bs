5a7914a170f0eacd83006253c2fde0727ad5eadc
hasifjasmi <hasifj2207@gmail.com>
2024-12-22 16:56:21 +0800

    test1

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -5,6 +5,8 @@ import useSound from "use-sound";
 export default function Meme() {
   const getout = "/GETOUT.mp3"; // Correctly reference the audio file
   const [play, { stop }] = useSound(getout);
+  const salam = "/assalam.mp3"; // Correctly reference the audio file
+  const [play1, { stop1 }] = useSound(salam);
 
   return (
     <>
@@ -23,6 +25,22 @@ export default function Meme() {
         >
           <span class="flex flex-col justify-center items-center h-full text-white font-bold text-lg ">
             GET OUT
+          </span>
+        </div>
+        <div
+          onClick={() => {
+            stop1();
+            play1();
+          }}
+          class="button w-40 h-16 bg-red-500 rounded-lg cursor-pointer select-none
+    active:translate-y-2  active:[box-shadow:0_0px_0_0_#b91c1c,0_0px_0_0_#b91c1c]
+    active:border-b-[0px]
+    transition-all duration-150 [box-shadow:0_10px_0_0_#b91c1c,0_15px_0_0_#b91c1c]
+    border-b-[1px] border-red-700
+  "
+        >
+          <span class="flex flex-col justify-center items-center h-full text-white font-bold text-lg ">
+            SALAM
           </span>
         </div>
       </div>
